f3f27ac47834a2062af5b653a532fde6562c4c35
Victor Hugo Romero Esquivel <vhre03@gmail.com>
2024-08-22 20:27:30 -0600

    Conexión del backend con el frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { TaskPage } from "./pages/TaskPage";
+import { TaskFormPage } from "./pages/TaskFormPage";
+
 function App() {
-  return <div>Hola Mundo</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/tasks" />} />
+        <Route path="/tasks" element={<TaskPage />} />
+        <Route path="/tasks-create" element={<TaskFormPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Frontend/src/pages/TaskFormPage.jsx
@@ -0,0 +1,3 @@
+export function TaskFormPage(params) {
+  return <div>Task Form Page</div>;
+}
